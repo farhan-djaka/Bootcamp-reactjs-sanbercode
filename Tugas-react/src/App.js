71fd3cbdaaf5f9bb8f507c4b889ccd2384e9f025
Farhan able <farhanroif51@gmail.com>
2022-10-14 07:51:53 +0700

    tugas 4

---
+++ Tugas-react/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./App.css"
+
+const App = () => {
+    return(<>
+    <nav className="navbar">
+    <h1>First React</h1>
+    <img alt="woman" src= "https://cdn.pixabay.com/photo/2017/05/11/08/48/woman-2303361__340.jpg"/>
+    </nav>
+    <hero />
+    <article />
+
+    </>
+    )
+}
+
+export default App
